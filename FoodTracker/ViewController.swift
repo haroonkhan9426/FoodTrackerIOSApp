bdd2fc3edd25850c91030cef928d2de64ad8272c
Haroon khan <haroonkhan9426@gmail.com>
2019-04-16 20:45:29 +0500

    ImageView and Image picker added

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -8,18 +8,45 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate {
+class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
     //Mark: Properties
     
     
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var mealNameLabel: UILabel!
     @IBOutlet weak var setDefaultButton: UIButton!
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+     let imagePickerController = UIImagePickerController()
+    
     
     //Mark: Actions
     @IBAction func setDefaultButtonAction(_ sender: UIButton) {
         mealNameLabel.text = "Defualt Text"
+        nameTextField.text = " "
     }
+    @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
+        nameTextField.resignFirstResponder();
+       
+        imagePickerController.sourceType = .photoLibrary
+        present(imagePickerController, animated: true, completion: nil)
+        
+    }
+    
+    //Mark: UIImagePickerControllerDelegate
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        print("@imagePickerControllerDidCancel: Called")
+        dismiss(animated: true, completion: nil)
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        print("@imagePickerController:Called")
+        guard let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage else {
+            fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
+        }
+        photoImageView.image = selectedImage
+        dismiss(animated: true, completion: nil)
+    }
+    
     
     //Mark: UITextFieldDelegate
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
@@ -39,6 +66,7 @@ class ViewController: UIViewController, UITextFieldDelegate {
         super.viewDidLoad()
         
         nameTextField.delegate = self
+        imagePickerController.delegate = self
         
        // print("Screen loaded")
         // Do any additional setup after loading the view, typically from a nib.
